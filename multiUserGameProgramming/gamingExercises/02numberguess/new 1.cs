e610136374da7a22646c03b83755fa0aaa90addb
Ronald Durham <70582212+S11654530@users.noreply.github.com>
2023-10-03 14:19:13 -0400

    Create new 1.cs

---
+++ multiUserGameProgramming/gamingExercises/02numberguess/new 1.cs
@@ -0,0 +1,30 @@
+//02_Colletions, Ronald Durham, 10-3-2023 v0.0
+using Systems;
+
+namespace _02_Collections 
+{
+    class Program 
+	(
+	        static void Main(string[] orgs)
+			(
+			   // Collections are variables that can store multiple values Inone variables
+			   
+			   /* Arrays 
+			   -- Number of elements in an array CANNOT change.
+			   -- Contents of elements In an array CAN change.
+			   -- Items In the array are called ELEMENTS 
+			   -- Arrays are ordered, meaning each items has a fixed position 
+			   -- The position is known as the INDEX 
+			   -- First element in array is index 0.
+			   */
+			   
+			   // Declaring and Defining an Array 
+			   string [] breakfastfoods - {"Bacon", "waffels", "Pancakes", "cereal" , "purfait"};
+			   int[] testScores - {95, 100, 25, 15, 27, 35};
+			   float[] GPA = (3.14f, 2.25f, 1.74f, 099f, 4.25f);
+			   
+			   
+			   
+			   
+			   )
+	)		   
